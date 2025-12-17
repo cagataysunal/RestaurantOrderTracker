3ab4681ea16f4e28f83ce9ff7dc510ede91eea6a
Çağatay Sunal <cagataysunal9199@gmail.com>
2025-12-17 19:59:50 +0300

    refactor: Reformat LoginUserUseCase constructor

--- app/src/main/java/com/cagataysunal/restaurantordertracker/domain/usecase/LoginUserUseCase.kt
+++ app/src/main/java/com/cagataysunal/restaurantordertracker/domain/usecase/LoginUserUseCase.kt
@@ -2,9 +2,7 @@ package com.cagataysunal.restaurantordertracker.domain.usecase
 
 import com.cagataysunal.restaurantordertracker.data.repository.UserRepository
 
-class LoginUserUseCase(
-    private val userRepository: UserRepository
-) {
+class LoginUserUseCase(private val userRepository: UserRepository) {
     suspend operator fun invoke(email: String, password: String) {
         userRepository.login(email, password)
     }
